793dff656687802f09acde5f51e4d29d799fa0e1
skj1991 <stacykjohns@icloud.com>
2020-08-16 23:16:29 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+function wakeDog(dogName, dogBreed) {
+    const action = `Wake ${dogName} the ${dogBreed}`;
+    console.log(action);
+    return action;
+}
+function leashDog(dogName, dogBreed) {
+    const action = `Leash ${dogName} the ${dogBreed}`;
+    console.log(action);
+    return action;
+}
+  
+function walkToPark(dogName, dogBreed) {
+    const action = `Walk to the park with ${dogName} the ${dogBreed}`;
+    console.log(action);
+    return action;
+}
+  
+function throwFrisbee(dogName, dogBreed) {
+    const action = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+    console.log(action);
+    return action;
+}
+  
+function walkHome(dogName, dogBreed) {
+    const action = `Walk home with ${dogName} the ${dogBreed}`;
+    console.log(action);
+    return action;
+}
+  
+function unleashDog(dogName, dogBreed) {
+    const action = `Unleash ${dogName} the ${dogBreed}`;
+    console.log(action);
+    return action;
+}
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+function exerciseDog(dogName, dogBreed) {
+    let personalRoutines = [];
+    for (let d = 0; d < routine.length; d++){
+        personalRoutines.push(routine[d](dogName, dogBreed));
+    }
+  return personalRoutines;
+}
+
+  
